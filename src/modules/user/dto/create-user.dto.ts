e5e233fa512686f7c198b89011c3c4726b333025
Khang1601 <dinhkhang1601@gmail.com>
2024-01-15 15:45:18 +0700

    first commit

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class CreateUserDTO {
+    @IsEmail()
+    email: string
+
+    @IsNotEmpty()
+    username: string
+
+    @IsNotEmpty()
+    password: string
+}
